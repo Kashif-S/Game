e743b662c79be2bbfaea1cf528f11d5e74b4a611
Arkadelic <shaiv08@hotmail.com>
2014-11-21 13:23:21 -0500

    modified noise

--- Rain/src/com/ready/rain/ImageWriter.java
+++ Rain/src/com/ready/rain/ImageWriter.java
@@ -12,7 +12,7 @@ public class ImageWriter {
 	protected static final Random random1 = new Random();
 	private static float rand;
 	private static float rand1;
-	private static float prevrand = 0.5f;
+	private static float prevrand = 0.1f;
 
     public static void greyWriteImage(double [][] data){
         //this takes and array of doubles between 0 and 1 and generates a grey scale image from them
@@ -27,8 +27,9 @@ public class ImageWriter {
         	
           for (int x = 0; x < data.length; x++)
           {
-        	  rand1= random1.nextInt(4);
-              rand1 -= Math.random()*prevrand;
+        	  rand1= 0.5f;
+        	  if(Math.random() >= 0.5)rand1 -= Math.random()*prevrand/10;
+        	  if(Math.random() < 0.5)rand1 += Math.random()*prevrand/10;
               prevrand = rand1;
         	  data[x][y] = rand1;
            if (data[x][y]>1){
@@ -37,10 +38,45 @@ public class ImageWriter {
             if (data[x][y]<0){
                 data[x][y]=0;
             }
-     
+            if (data[x][y]<0 && data[x][y]>0.1){
+                data[x][y]=0.05;
+            }
+            if (data[x][y]<0.1 && data[x][y]>0.2){
+                data[x][y]=0.15;
+            }
+            if (data[x][y]<0.2 && data[x][y]>0.3){
+                data[x][y]=0.25;
+            }
+            if (data[x][y]<0.3 && data[x][y]>0.4){
+                data[x][y]=0.35;
+            }
+            if (data[x][y]<0.4 && data[x][y]>0.5){
+                data[x][y]=0.45;
+            }
+            if (data[x][y]<0.5 && data[x][y]>0.6){
+                data[x][y]=0.55;
+            }
+            if (data[x][y]<0.6 && data[x][y]>0.7){
+                data[x][y]=0.65;
+            }
+            if (data[x][y]<0.7 && data[x][y]>0.8){
+                data[x][y]=0.75;
+            }
+            if (data[x][y]<0.8 && data[x][y]>0.9){
+                data[x][y]=0.85;
+            }
+            if (data[x][y]<0.9 && data[x][y]>1){
+                data[x][y]=0.95;
+            }
+
+            
+            
+            
+            
               Color col=new Color((float)data[x][y],(float)data[x][y],(float)data[x][y]); 
             image.setRGB(x, y, col.getRGB());
           }
+          
         }
 
         try {
